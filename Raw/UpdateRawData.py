16074a5226d6e450c75c5981b6b526ecfe5dc206
Nick Dagostino <nick.dagostino@preludecapital.com>
2023-04-21 12:39:01 -0400

    set up complete

--- Raw/UpdateRawData.py
+++ Raw/UpdateRawData.py
@@ -23,30 +23,32 @@ def get_previous(df: pd.DataFrame):
 def get_current():
     return datetime.now().strftime('%Y-%m-%d')
 
-def update_historical_data(tickers: list, startDate: str, endDate: str, df_old: pd.DataFrame, df_extraInfo: pd.DataFrame) -> pd.DataFrame:
+def update_historical_data(df_addInfo: pd.DataFrame, df_old: pd.DataFrame, startDate: str, endDate: str) -> pd.DataFrame:
     """
     Ingest and log historical data for SnP500 
     """
-    lastDate = endDate.replace('-', '')
-    logPath = r'Logs' + '\\' + lastDate + r'.log'
+    logPath = r'Logs' + '\\' + endDate.replace('-', '') + r'.log'
     temp = []
     logging.basicConfig(filename=logPath, level=logging.INFO, format='%(asctime)s %(name)s %(levelname)s %(message)s')
-    for tic in tickers:
+    for symbol, security, sector, sub_industry, cik in zip(df_addInfo['Symbol'].values, df_addInfo['Security'].values, df_addInfo['GICS Sector'].values, df_addInfo['GICS Sub-Industry'].values, df_addInfo['CIK'].values):
         try:
-            yf_ticker = yf.Ticker(tic)
-            df_historical = pd.DataFrame(yf_ticker.history(start= startDate, end= endDate, interval= '1d', auto_adjust= True))
-            df_historical.index = df_historical.index.tz_localize(None)
-            df_historical.insert(0, 'Symbol', tic)
-            temp.append(df_historical)
-            logging.info(f'Successfully retrieved {tic} data')
-            if len(df_historical) == 0:
+            yf_ticker = yf.Ticker(symbol)
+            df_historicalData = pd.DataFrame(yf_ticker.history(start= startDate, end= endDate, interval= '1d', auto_adjust= True))
+            df_historicalData.index = df_historicalData.index.tz_localize(None)
+            df_historicalData['Symbol'] = symbol
+            df_historicalData['Security'] = security
+            df_historicalData['GICS Sector'] = sector
+            df_historicalData['GICS Sub-Industry'] = sub_industry
+            df_historicalData['CIK'] = cik
+            temp.append(df_historicalData[['Symbol', 'Security', 'GICS Sector', 'GICS Sub-Industry', 'CIK'] + df_historicalData.columns[:-5].tolist()])
+            logging.info(f'Successfully retrieved {symbol} data')
+            if len(df_historicalData) == 0:
                 raise Exception('No data found')
         except Exception as e:
-            logging.error(f'Error getting data for {tic}: {e}')
+            logging.error(f'Error getting data for {symbol}: {e}')
             if str(e) == 'No data found':
-                logging.error(f'Error: No data received for {tic}: {e}')
+                logging.error(f'Error: No data received for {symbol}: {e}')
     df_new = pd.concat(temp).reset_index()
-    df_mergeExtraInfo = pd.merge(df_new, df_extraInfo, on='Symbol')
     df_updated = pd.concat([df_old, df_new]).reset_index(drop=True)
     df_updated = df_updated.sort_values(by='Date')
     return df_updated
@@ -55,17 +57,15 @@ def main():
     """
     Read in tickers and call methods to read and write data
     """
-    SnP500List = pd.read_html(r'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies', converters= {'CIK': str})
-    filterData = SnP500List[0]
-    sortData = filterData.sort_values(by=['Symbol'], ascending=True).astype('string')
+    SnP500List = pd.read_html(r'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies', converters= {'CIK': str})[0]
+    sortData = SnP500List.sort_values(by=['Symbol'], ascending=True).astype('string')
     sortData['Symbol'] = sortData['Symbol'].str.replace('.','-', regex=True)
-    tickers = sortData['Symbol'].tolist()
     additionalInfo = sortData[['Symbol', 'Security', 'GICS Sector', 'GICS Sub-Industry', 'CIK']]
     oldData = pd.read_excel(r'Raw\RawData.xlsx', converters= {'CIK': str})
     startDate = str(get_previous(oldData))
     endDate = str(get_current())
-    df_updatedData = update_historical_data(tickers, startDate, endDate, oldData, additionalInfo)
-    write_out(df_updatedData, r'Raw\test.xlsx')
+    df_updatedData = update_historical_data(additionalInfo, oldData, startDate, endDate)
+    write_out(df_updatedData, r'Raw\RawData.xlsx')
 
 if __name__ == '__main__':
     main()
