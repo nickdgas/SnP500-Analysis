7ed516a43af94d3a0922cf45ce6872c87dcd7553
Nicholas D'Agostino <nickdags123@gmail.com>
2023-04-24 17:10:29 -0400

    Creating new table (#8)
    * initializing type table
    
    * new model
    
    ---------
    
    Co-authored-by: Nick Dagostino <nick.dagostino@preludecapital.com>

--- Market/Market.py
+++ Market/Market.py
@@ -17,11 +17,14 @@ def main():
     df_RawData = pd.read_excel(r'Raw\RawData.xlsx', converters= {'CIK': str})  
     df_Security = pd.read_excel(r'Security\Security.xlsx', usecols=['ID', 'Short Name']).rename(columns={'ID': 'SecurityID'})
     df_Date = pd.read_excel(r'Date\Date.xlsx').rename(columns={'ID': 'ReportDateID'})
+    #df_Type = pd.read_excel(r'Type\Type.xlsx').rename(columns={'ID': 'TypeID'})
     df_SecurityMerge = df_RawData.merge(df_Security, left_on='Symbol', right_on='Short Name')
     df_DateMerge = df_SecurityMerge.merge(df_Date, left_on='Date', right_on='Date')
+    #df_TypeMerge = df_DateMerge.merge(df_Type)
     df_Market = df_DateMerge[['SecurityID', 'ReportDateID', 'Open', 'High', 'Low', 'Close', 'Volume', 'Dividends', 'Stock Splits']]
     df_Market.insert(0, 'ID', df_Market.index+1)
-    write_out(df_Market, r'Market\Market.xlsx')
+    #write_out(df_Market, r'Market\Market.xlsx')
+    #print(df_Type)
 
 if __name__ == '__main__':
     main()
