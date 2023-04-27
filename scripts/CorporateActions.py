60db4c30e2b0e1636e43e18c79d6b35c698e7bdb
Nicholas D'Agostino <nickdags123@gmail.com>
2023-04-27 13:33:03 -0400

    Corporate actions pivot table (#12)
    * edits tot corp act
    
    * corpo actions merged to securities
    
    * edit
    
    * edits
    
    ---------
    
    Co-authored-by: Nick Dagostino <nick.dagostino@preludecapital.com>

--- scripts/CorporateActions.py
+++ scripts/CorporateActions.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import datetime
 import xlsxwriter
 
 def write_out(df: pd.DataFrame, output: str):
@@ -19,15 +20,14 @@ def main():
     df_RawData = pd.read_excel(r'data\raw\RawData.xlsx', converters= {'CIK': str}, usecols=['Date', 'Symbol', 'Dividends', 'Stock Splits'])
     # normalize data; rename other columns; insert ID column
     df_RawData['Is Current'] = np.where((df_RawData['Dividends'] != 0) | (df_RawData['Stock Splits'] != 0), 1, 0)
-    current = df_RawData['Date'].max()
+    current = pd.Timestamp.max.strftime('%Y-%m-%d')
     df_Drop = df_RawData.loc[df_RawData['Is Current'] == 1] 
-    df_Filter = df_Drop[['Symbol', 'Is Current', 'Date']].sort_values(by='Symbol')
-    df_Pivot = pd.pivot_table(df_Filter, values='Date', index='Symbol', aggfunc=['min','max'])
-    df_Pivot.columns = [f'{col[1]}{col[0]}' for col in df_Pivot.columns]
-    df_Pivot = df_Pivot.reset_index()
+    df_Filter = df_Drop[['Symbol', 'Dividends', 'Stock Splits', 'Is Current', 'Date']].rename(columns={'Date': 'Start Date'})
+    df_Filter['Is Current'] = (df_Filter.groupby('Symbol')['Start Date'].transform('last') == df_Filter['Start Date']).astype(int)
+    df_Filter['End Date'] = df_Filter.groupby('Symbol')['Start Date'].transform('last')
+    df_Filter.loc[df_Filter['Is Current'] == 1, 'End Date'] = current
     # write to excel
-    #write_out(df_filterDupes, r'data\processed\CorporateActions.xlsx')
-    print(df_Pivot)
+    write_out(df_Filter, r'data\processed\CorporateActions.xlsx')
 
 if __name__ == '__main__':
     main()
