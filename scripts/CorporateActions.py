b14706e4cfd21b7dbc6277e903ec7384fcb93227
Nicholas D'Agostino <nickdags123@gmail.com>
2023-04-27 14:24:50 -0400

    model fixed; actions table finished (#13)
    Co-authored-by: Nick Dagostino <nick.dagostino@preludecapital.com>

--- scripts/CorporateActions.py
+++ scripts/CorporateActions.py
@@ -16,15 +16,17 @@ def main():
     """
     Calls methods to read and write data
     """    
-    # read security data as type string
+    # read raw and security data
     df_RawData = pd.read_excel(r'data\raw\RawData.xlsx', converters= {'CIK': str}, usecols=['Date', 'Symbol', 'Dividends', 'Stock Splits'])
-    # normalize data; rename other columns; insert ID column
-    df_RawData['Is Current'] = np.where((df_RawData['Dividends'] != 0) | (df_RawData['Stock Splits'] != 0), 1, 0)
+    df_Security = pd.read_excel(r'data\processed\Security.xlsx', usecols=['ID', 'Short Name']).rename(columns={'ID': 'SecurityID'})
+    # normalize data, insert columns; merge using Security ID
+    df_SecurityMerge = df_RawData.merge(df_Security, left_on='Symbol', right_on='Short Name')
+    df_SecurityMerge['Is Current'] = np.where((df_SecurityMerge['Dividends'] != 0) | (df_SecurityMerge['Stock Splits'] != 0), 1, 0)
     current = pd.Timestamp.max.strftime('%Y-%m-%d')
-    df_Drop = df_RawData.loc[df_RawData['Is Current'] == 1] 
-    df_Filter = df_Drop[['Symbol', 'Dividends', 'Stock Splits', 'Is Current', 'Date']].rename(columns={'Date': 'Start Date'})
-    df_Filter['Is Current'] = (df_Filter.groupby('Symbol')['Start Date'].transform('last') == df_Filter['Start Date']).astype(int)
-    df_Filter['End Date'] = df_Filter.groupby('Symbol')['Start Date'].transform('last')
+    df_Drop = df_SecurityMerge.loc[df_SecurityMerge['Is Current'] == 1] 
+    df_Filter = df_Drop[['SecurityID', 'Dividends', 'Stock Splits', 'Is Current', 'Date']].rename(columns={'Date': 'Start Date'})
+    df_Filter['Is Current'] = (df_Filter.groupby('SecurityID')['Start Date'].transform('last') == df_Filter['Start Date']).astype(int)
+    df_Filter['End Date'] = df_Filter.groupby('SecurityID')['Start Date'].transform('last')
     df_Filter.loc[df_Filter['Is Current'] == 1, 'End Date'] = current
     # write to excel
     write_out(df_Filter, r'data\processed\CorporateActions.xlsx')
